e5ac3ce212fca591ace55c36217d74df30c1a0b9
Kenan Atmaca <loveiscode@gmail.com>
2019-04-01 00:58:36 +0300

    Create RawString.swift

---
+++ Examples/RawString.swift
@@ -0,0 +1,9 @@
+// kenanatmaca.com
+
+let name = "Kenan"
+let rawStr = #"Hi! my name is "\#(name)""#
+
+let multiRawStr = #"""
+Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
+The passage experienced a surge in popularity during the "1960s" when Letraset used it on their dry-transfer sheets, and again during the "90s" as desktop publishers bundled the text with their software.
+"""#
